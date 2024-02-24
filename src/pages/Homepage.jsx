fb31a09298d5cae0b937f8f75a3665b114855c71
sergiobni <bni.sergio@gmail.com>
2024-02-24 18:00:09 +0100

    styling and styled modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import PageNav from '../Components/PageNav';
+import AppNav from '../Components/AppNav';
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Homepage</h1>
-      <Link to="/pricing">Pricing</Link>
+      <Link to="/app">go to app</Link>
     </div>
   );
 }
